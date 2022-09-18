4d976c7915145f29fd35d3844e7ce40253faa405
Bhushan-Wankhede <113833717+Bhushan-Wankhede@users.noreply.github.com>
2022-09-18 16:43:05 +0530

    Create Addition.py

---
+++ Addition.py
@@ -0,0 +1,9 @@
+## How to do addittion on two variables. 
+print("Enter the First Value")
+a = input()
+print("Enter the Second Value")
+b = input ()
+## add a + b into c and print C
+c = int(a)+int(b)
+print("Total of Above two no is")
+print(c)
